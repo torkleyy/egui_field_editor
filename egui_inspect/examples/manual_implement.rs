90fe9e1082cd58ba20ad2f57778e09fa504f4ad5
Ultrasuperingu <ultrapingu@gmail.com>
2025-08-09 22:45:51 +0200

    Fix Doc

--- egui_inspect/examples/manual_implement.rs
+++ egui_inspect/examples/manual_implement.rs
@@ -1,15 +1,6 @@
 use egui::{Color32, TextEdit};
 use egui_inspect::EguiInspector;
 
-struct MyStruct {
-	a_bool:bool,
-	an_int:i32,
-	an_uint:u64,
-	a_float:f32,
-	a_color:egui::Color32,
-	a_string:String,
-	a_second_string:String,
-}
 impl Default for MyStruct {
 	fn default() -> Self {
 		Self { a_bool: Default::default(), an_int: Default::default(), an_uint: Default::default(), a_float: Default::default(), a_color: Color32::from_rgb(127, 0, 200), a_string: String::from("A single line string"), a_second_string: String::from("A\nmultiline\nline\nstring") }
@@ -29,6 +20,15 @@ impl eframe::App for MyStruct {
 	}
 }
 
+struct MyStruct {
+	a_bool:bool,
+	an_int:i32,
+	an_uint:u64,
+	a_float:f32,
+	a_color:egui::Color32,
+	a_string:String,
+	a_second_string:String,
+}
 impl egui_inspect::EguiInspect for MyStruct {
 	fn inspect_with_custom_id(&mut self, _parent_id: egui::Id, label: &str, _tooltip: &str, read_only: bool, ui: &mut egui::Ui) {
 		let id = if _parent_id == egui::Id::NULL { ui.next_auto_id() } else { _parent_id.with(label) };
